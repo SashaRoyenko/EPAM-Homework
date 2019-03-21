4e948e00fe007a9fc44b7636b0de43abe6a4499b
SashaRoyenko <sasharoyenko@gmail.com>
2019-03-21 22:35:43 +0200

    Added task 4 with recursion and easy understanding

--- Lesson_1/Task 1/src/com/company/Main.java
+++ Lesson_1/Task 1/src/com/company/Main.java
@@ -97,6 +97,41 @@ public class Main {
         System.out.println("GSD: " + (firstNumber << result));
     }
 
+    public static int gcd2(int firstNumber, int secondNumber) {
+        /*
+            Алгоритм
+            1. НОД(0, n) = n; НОД(m, 0) = m;
+            2. Если m, n чётные, тогда НОД(m, n) = 2 * НОД(m / 2, n / 2).
+            3. Если m чётное, тогда НОД(m, n) = НОД(m / 2, n).
+            4. Если n чётное, тогда НОД(m, n) = НОД(m, n / 2).
+            5. Если m, n нечётные и m > n, тогда НОД(m, n) = НОД(n, m - n).
+            6. Если m, n нечётные и m < n, тогда НОД(m, n) = НОД(n, n - m).
+            7. Если m = n, тогда НОД(m, n) = m;
+        */
+        if (firstNumber == 0 || secondNumber == 0) {
+            return firstNumber | secondNumber;
+        }
+        if ((firstNumber & secondNumber) == firstNumber) {
+            return firstNumber;
+        }
+//        if ((firstNumber & 1) == 0 && (secondNumber & 1) == 0) {
+//            return gcd2(firstNumber >> 1, secondNumber >> 1) << 1;
+//        } else if ((firstNumber & 1) == 0) {
+//            return gcd2(firstNumber >> 1, secondNumber);
+//        } else if ((secondNumber & 1) == 0) {
+//            return gcd2(firstNumber, secondNumber >> 1);
+//        } else if ((firstNumber & 1) != 0 && (secondNumber & 1) != 0 && firstNumber > secondNumber) {
+//            return gcd2(secondNumber, firstNumber - secondNumber);
+//        } else {
+//            return gcd2(secondNumber, secondNumber - firstNumber);
+//        }
+        if ((firstNumber & 1) == 0) {
+            return ((secondNumber & 1) == 0) ? (gcd2(firstNumber >> 1, secondNumber >> 1) << 1) : gcd2(firstNumber >> 1, secondNumber);
+        } else {
+            return ((secondNumber & 1) == 0) ? gcd2(firstNumber, secondNumber >> 1) : gcd2(secondNumber, (firstNumber > secondNumber) ? firstNumber - secondNumber : secondNumber- firstNumber);
+        }
+    }
+
     public static void changeBit(int number, int position) {
         System.out.println("Number before change bit: " + Integer.toBinaryString(number) + " " + number);
         number ^= (1 << position);
@@ -110,7 +145,7 @@ public class Main {
         String binaryFormat = scan.next();
         transformToBinary(binaryFormat);
         //Second task
-        System.out.print("Input number which sign you want to transform into positive: ");
+        System.out.print("Input number which sign you want to transform: ");
         int negativeNumber = scan.nextInt();
         changeSign(negativeNumber);
         //Third task
@@ -121,22 +156,14 @@ public class Main {
         System.out.print("Input second number: ");
         int secondNumber = scan.nextInt();
         gcd(firsNumber, secondNumber);
+        System.out.println("GSD: " + gcd2(firsNumber, secondNumber));
         //Fifth task
         System.out.print("Input number which bit you want to change: ");
         int numberToChange = scan.nextInt();
         System.out.print("Input position of bit which you want to change: ");
         int bitPosition = scan.nextInt();
         changeBit(numberToChange, bitPosition);
-//            int i = 1;
-//            Object object = new Object();
-//            Main memory = new Main();
-//            memory.exMethod(object);
 
-    }
-
-    public void exMethod(Object param) {
-        String string = param.toString();
-        System.out.println(string);
     }
 
 }
